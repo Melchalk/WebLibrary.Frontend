a976087fbfb4bfc2d5303b3e68011b108d4dea91
Marina Mel <melm2004@mail.ru>
2024-05-16 12:02:02 +0300

    add get info

---
+++ src/components/BooksTable.tsx
@@ -0,0 +1,33 @@
+import { Table } from "react-bootstrap";
+import { GetBookResponse } from "../api/BookApi";
+
+export default function BooksTable(stateResponse: GetBookResponse[]){
+    return(
+        <Table className="mx-auto" striped bordered hover>
+        <thead>
+            <tr>
+            <th>Id</th>
+            <th>Title</th>
+            <th>Author</th>
+            <th>NumberPages</th>
+            <th>YearPublishing</th>
+            <th>CityPublishing</th>
+            <th>IssueId</th>
+            </tr>
+        </thead>
+        <tbody>
+            {stateResponse?.map((item:GetBookResponse) =>
+                <tr>
+                    <td>{item.id}</td>
+                    <td>{item.title}</td>
+                    <td>{item.author}</td>
+                    <td>{item.numberPages}</td>
+                    <td>{item.yearPublishing}</td>
+                    <td>{item.cityPublishing}</td>
+                    <td>{item.issueId != null ? item.issueId : "Находится в библиотеке"}</td>
+                </tr>
+            )}
+        </tbody>
+    </Table>
+    )
+}
--- src/pages/BookPage.tsx
+++ src/pages/BookPage.tsx
@@ -1,18 +1,10 @@
-import { useState } from "react";
-import { Table } from "react-bootstrap";
-import { CreateBookRequest, GetBookResponse } from "../api/BookApi";
+import { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
+import { CreateBookRequest, GetBookResponse, getBooks } from "../api/BookApi";
+import BooksTable from "../components/BooksTable";
 
 export default function BookPage(){
-    const [stateResponse, setStateResponse] = useState<GetBookResponse>({
-        id: '',
-        title: '',
-        author: null,
-        numberPages: 0,
-        yearPublishing: 0,
-        cityPublishing: null,
-        issueId: null,
-        hallNo: null
-    });    
+    const [stateResponse, setStateResponse] = useState<GetBookResponse[]>();    
 
     const [stateCreateRequest, setStateCreateRequest] = useState<CreateBookRequest>({
         title: '',
@@ -23,35 +15,30 @@ export default function BookPage(){
         hallNo: null
     });    
 
+    const [showToast, setShowToast] = useState(false);
+    const [errorMessage, setError] = useState<any>();
+
+    useEffect(() => {
+        getBooks()
+            .then((res) =>{
+                setStateResponse(res.data);
+            })
+            .catch((error) => {
+                setShowToast(true);
+                if (error.response) {
+                    setError(error.response.data);
+                } else if (error.request) {
+                    setError(error.request);
+                } else {
+                    setError(error.message);
+            }})
+    }, []);  
+
     return(
-        <Table className="mx-auto" striped bordered hover>
-            <thead>
-                <tr>
-                <th>#</th>
-                <th>First Name</th>
-                <th>Last Name</th>
-                <th>Username</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                <td>1</td>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-                </tr>
-                <tr>
-                <td>2</td>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-                </tr>
-                <tr>
-                <td>3</td>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-                </tr>
-            </tbody>
-        </Table>
+        <>
+        <Button variant="warning" className="col-md-1.5 mb-3">Создать книгу</Button>
+
+        {BooksTable(stateResponse!)}
+        </>
     )
 }
